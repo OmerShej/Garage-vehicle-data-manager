65ad000ab458d60b8baf91c85350df14be28d2a2
OmerShej <81814395+OmerShej@users.noreply.github.com>
2021-04-03 13:44:37 +0300

    Add files via upload

---
+++ Ex03.ConsoleUI/MenuOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03.ConsoleUI
+{
+    enum eMenuOptions
+    {
+        OptionOne = 1,
+        OptionTwo = 2,
+        OptionThree = 3,
+        OptionFour = 4,
+        OptionFive = 5,
+        OptionSix = 6,
+        OptionSeven = 7,
+        OptionEight = 8
+    }
+}
